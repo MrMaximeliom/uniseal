483d66b2fa8b21a06c1ad71321e62e8339c68dfa
Moayed <hysoca7@gmail.com>
2021-12-23 12:38:27 +0200

    Upgrading packages

--- slider/urls.py
+++ slider/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from slider import dashboard_views as slider_views
 
 urlpatterns = [
-path('allSliders', slider_views.all_sliders, name='allSliders'),
+    path('allSliders', slider_views.all_sliders, name='allSliders'),
     path('addSliders', slider_views.add_sliders, name='addSliders'),
     path('deleteSliders', slider_views.delete_sliders, name='deleteSliders'),
     path('editSliders', slider_views.edit_sliders, name='editSliders'),
