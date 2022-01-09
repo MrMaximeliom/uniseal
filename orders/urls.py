4527710fe49edab7732cf40d6661ed420e86e68e
moayed <hysoca7@gmail.com>
2022-01-09 21:53:54 +0200

    last commit at home

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,5 @@ from orders import dashboard_views as orders_views
 
 urlpatterns = [
     path('allOrders', orders_views.all_orders, name='allOrders'),
-
-    ]
+    path('orderDetails/<str:slug>', orders_views.edit_order, name='editOrder'),
+]
