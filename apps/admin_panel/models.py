c98560b15bbe40ffcdb21169c2fec6cac431735c
moayed <hysoca7@gmail.com>
2022-02-10 10:55:26 +0200

    altering admin panel models

--- apps/admin_panel/models.py
+++ apps/admin_panel/models.py
@@ -4,13 +4,13 @@ from django.utils.translation import gettext_lazy as _
 class ManageProductsPage(models.Model):
     user = models.ForeignKey(
         "accounts.User",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('User'),
-        null=True,
-        blank=False,
+
     )
     product_page_views = models.PositiveIntegerField(
         verbose_name=_('Product Page Views'),
+        default=0
 
 
     )
@@ -20,128 +20,123 @@ class ManageProductsPage(models.Model):
 class ManageProducts(models.Model):
     product = models.ForeignKey(
         "product.Product",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('Product'),
-        null=True,
-        blank=False,
+
     )
     user = models.ForeignKey(
         "accounts.User",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('User'),
-        null=True,
-        blank=False,
+
     )
     product_views = models.PositiveIntegerField(
         verbose_name=_('Product Views'),
         blank=True,
-        null=True,
+        default=0
     )
     product_sheet_downloads = models.PositiveIntegerField(
         verbose_name=('Product Sheet Downloads'),
         blank=True,
-        null=True,
+        default=0
 
     )
 
 class ManageProjects(models.Model):
     project = models.ForeignKey(
         "project.Project",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('Project'),
-        null=True,
-        blank=False,
+
     )
     user = models.ForeignKey(
         "accounts.User",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('User'),
-        null=True,
-        blank=False,
+
     )
     project_views = models.PositiveIntegerField(
         verbose_name=_('Project Views'),
+        default=0
     )
 
 class ManageSolution(models.Model):
     solution = models.ForeignKey(
         "solution.Solution",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('Soltion'),
-        null=True,
-        blank=False,
+
     )
     user = models.ForeignKey(
         "accounts.User",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('User'),
-        null=True,
-        blank=False,
+
     )
     solution_views = models.PositiveIntegerField(
         verbose_name=_('Solution Views'),
+        default=0
     )
 
 class ManageSellingPoints(models.Model):
     selling_point = models.ForeignKey(
         "sellingPoint.SellingPoint",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('Selling Point'),
-        null=True,
-        blank=False,
+
     )
     user = models.ForeignKey(
         "accounts.User",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('User'),
-        null=True,
-        blank=False,
+
     )
     phone_number_clicks = models.PositiveIntegerField(
         verbose_name=_('Phone Number Clicks'),
+        default=0
     )
     secondary_phone_number = models.PositiveIntegerField(
         verbose_name=_('Secondary Phone Number Clicks'),
+        default=0
     )
 
 class ManageBrochures(models.Model):
     brochures = models.ForeignKey(
         "brochures.Brochures",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('Brochure'),
-        null=True,
-        blank=False,
+
     )
     user = models.ForeignKey(
         "accounts.User",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('User'),
-        null=True,
-        blank=False,
+
     )
     brochures_views = models.PositiveIntegerField(
         verbose_name=_('Brochures_views'),
+        default=0
     )
     brochures_sheet_downloads = models.PositiveIntegerField(
-        verbose_name=('Brochures Sheet Downloads')
+        verbose_name=('Brochures Sheet Downloads'),
+        default=0
     )
 
 class ManageCarts(models.Model):
     cart = models.ForeignKey(
         "orders.Cart",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('Carts'),
-        null=True,
-        blank=False,
+
     )
     user = models.ForeignKey(
         "accounts.User",
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         verbose_name=_('User'),
-        null=True,
-        blank=False,
+
     )
     add_to_cart_views = models.PositiveIntegerField(
         verbose_name=_('Add To Cart Views'),
+        default=0
     )
 
