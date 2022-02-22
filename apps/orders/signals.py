98518b569a6389d87d891636c4a5bd1dc9035479
Moayed <hysoca7@gmail.com>
2022-02-22 14:53:11 +0200

    update signals

--- apps/orders/signals.py
+++ apps/orders/signals.py
@@ -1,26 +1,15 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.shortcuts import get_object_or_404
-from apps.orders.models import Order
+from apps.orders.models import Order,Cart
 
-
-# from django.contrib.auth import user_logged_in, user_logged_out
-# from django.dispatch.dispatcher import receiver as receiver_second
-# from django.contrib.sessions.models import Session
-
-
-# عند تسجيل مستخدم جديد يتم إنشاء ملف شخصي خاص به تلقائيا
-@receiver(post_save, sender=Order)
-def create_sms_contact(sender, instance, created, **kwargs):
-    from apps.orders.models import Cart
-    carts = Cart.objects.filter(order=instance)
-    total = 0.0
+@receiver(post_save, sender=Cart)
+def calculate_total_contact(sender, instance, created, **kwargs):
     if created:
-        for cart in carts:
-            total_cart_price = cart.product.price * cart.quantity
-            total = total_cart_price+ total
-        instance.total = total
-        instance.save()
+        order  = Order.objects.get(id=instance.order.id)
+        order.total += instance.product.price * instance.quantity
+        order.save()
+
+
 
 
 
