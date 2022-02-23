0bcd1eaad2da471f28fb6f8bf0a230d29a1a2564
Moayed <hysoca7@gmail.com>
2022-02-23 08:27:40 +0200

    update signals

--- apps/orders/signals.py
+++ apps/orders/signals.py
@@ -1,14 +1,21 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,pre_delete
 from django.dispatch import receiver
 from apps.orders.models import Order,Cart
 
 @receiver(post_save, sender=Cart)
-def calculate_total_contact(sender, instance, created, **kwargs):
+def calculate_total_amount(sender, instance, created, **kwargs):
     if created:
         order  = Order.objects.get(id=instance.order.id)
         order.total += instance.product.price * instance.quantity
         order.save()
 
+
+
+@receiver(pre_delete, sender=Cart)
+def reduce_total_amount(sender, instance, **kwargs):
+        order  = Order.objects.get(id=instance.order.id)
+        order.total -= instance.product.price * instance.quantity
+        order.save()
 
 
 
