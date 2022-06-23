ec463bf42c908f8028cc2be88c97847e752d9720
moayed <hysoca7@gmail.com>
2022-06-23 13:02:03 +0200

    rapping up - final commit

--- unisealAPI/settings.py
+++ unisealAPI/settings.py
@@ -144,8 +144,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':3,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
